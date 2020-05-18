2a2b3d886be00a5d7bd2eb182501fe1b848e0a14
Pascal S <pascal.senjic@gmail.com>
2020-05-19 00:14:10 +0200

    data preparation

--- tf/constraints/schedule.py
+++ tf/constraints/schedule.py
@@ -7,3 +7,12 @@ import pandas as pd
 raw_data = pd.read_csv(filepath_or_buffer="./data/researchers.csv")
 print(raw_data.head())
 
+# transfer raw_data into a sample data
+# 1. generate one line containing all researchers data
+#    one line contains researchers data for all reserachers
+#    r1_is_earliest,r1_is_latest,etc.
+dataframes = {}
+# get all researchers only dataframes
+for researcher in raw_data["researcher"].unique():
+    dataframes[researcher] = raw_data.query("researcher == '"+researcher+"'")
+    print(dataframes[researcher].head())
