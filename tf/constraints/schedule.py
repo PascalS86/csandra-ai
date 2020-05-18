92e025263f6c752ceddac28c33ea22b56ef84481
Pascal S <pascal.senjic@gmail.com>
2020-05-18 23:51:25 +0200

    read data

--- tf/constraints/schedule.py
+++ tf/constraints/schedule.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
-
+import pandas as pd
 # reminder:
 # https://github.com/google-research/tensorflow_constrained_optimization/blob/master/examples/jupyter/Recall_constraint.ipynb 
+
+# read the researchers sample base data into a raw_data dataframe
+raw_data = pd.read_csv(filepath_or_buffer="./data/researchers.csv")
+print(raw_data.head())
+
